7275b9e82dae7a5a1fdf905b88f03a0ee2675045
CHOI0212 <choiys0311@gmail.com>
2020-03-26 22:11:20 +0900

    vote model

--- vote/models.py
+++ vote/models.py
@@ -5,6 +5,6 @@ class VoteList(models.Model):
     user = models.OneToOneField(User,on_delete = models.CASCADE)
     myname = models.CharField(max_length=10,blank=True)
     name = models.CharField(max_length=10,blank=True)
-
+    votenum = models.IntegerField(default=0)
     def __str__(self):
         return self.myname
--- vote/views.py
+++ vote/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from account.models import Profile
 from .models import VoteList
 from django.contrib.auth.models import User
+
 def vote(request):
   picks = Profile.objects
   return render(request, "vote/list.html",{'picks':picks})
 
 def complete(request):
-  myvote = VoteList()
-  myvote.user = request.user
-  myvote.myname = request.user.username
-  myvote.name = request.POST.get('pick')
-  myvote.save()
-  return render(request,"vote/complete.html")
+    nowuser = VoteList.objects.filter(user=request.user)
+    if not nowuser:
+      myvote = VoteList()
+      myvote.user = request.user
+      myvote.myname = request.user.username
+      myvote.name = request.POST.get('pick')
+      myvote.votenum += 1
+      myvote.save()
+    else:
+      obj = VoteList.objects.get(user = request.user)
+      obj.name = request.POST.get('pick')
+      obj.votenum += 1
+      obj.save()
+    return render(request,"vote/complete.html")

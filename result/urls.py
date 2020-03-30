b428fc9b4cf16a18bc091b256c876de68ff25fa7
CHOI0212 <choiys0311@gmail.com>
2020-03-31 00:54:09 +0900

    add beforeteam button

--- result/urls.py
+++ result/urls.py
@@ -3,4 +3,5 @@ from . import views
     
 urlpatterns = [
     path('',views.result,name='result'),
+    path('before',views.beforeteam,name='beforeteam'),
 ]
--- result/views.py
+++ result/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from vote.models import VoteList
 
 def result(request):
     votes = VoteList.objects
     return render(request,'result/result.html',{'votes':votes})
+
+def beforeteam(request):
+    votes = VoteList.objects.all()
+    for vote in votes:
+        vote.lastteam = vote.team
+        vote.save()
+    return redirect('result')
+    

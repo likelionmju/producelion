ebb86867b994a655a7d4de8235ad2f302b1d106e
CHOI0212 <choiys0311@gmail.com>
2020-04-01 21:58:15 +0900

    heroku

--- config/settings.py
+++ config/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nn^+5x7e2@mo92%v(l1v!i0@(k@28kwmj&mc6&y7l@o#0%_!+0'
-
+#SECRET_KEY = 'nn^+5x7e2@mo92%v(l1v!i0@(k@28kwmj&mc6&y7l@o#0%_!+0'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ["*"]
 
